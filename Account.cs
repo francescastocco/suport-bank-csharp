d9caed6b9553b5185bfe274fd57d0d3a57f7eefa
Francesca Stocco <francescaj.stocco@gmail.com>
2021-06-29 15:52:21 +0100

    reading csv file

---
+++ Account.cs
@@ -0,0 +1,8 @@
+// namespace SupportBank
+// {
+//     public class Account {
+//         public string name { get; set; }
+//         public List<Transaction> transactionHistory { get; set; }
+        
+//     }
+// }
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.ReadAllLines("./Transactions2014.csv");
+            
+            foreach (var line in file) {
+                var cells = line.Split(",");
+                
+            }
+
+            for(int i = 1; i < file.Length; i++) {
+                var cells = file[i].Split(",");
+                int amountFloat = int.Parse(cells[4]);
+
+                var transaction = new Transaction(cells[0], amountInt, cells[3], cells[1], cells[2], i);
+
+
+                cells.push(
+                    new Transaction {}
+                )
+            }
+          
+        }
+
+    }
+
+}
+
